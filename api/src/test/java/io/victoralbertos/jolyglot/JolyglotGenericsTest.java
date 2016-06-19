7a0e042a99bea527e0c4e477190fafba164d881f
Victor <victor@refineriaweb.com>
2016-06-19 20:42:42 +0200

    Added type collections tests.

--- api/src/test/java/io/victoralbertos/jolyglot/JolyglotGenericsTest.java
+++ api/src/test/java/io/victoralbertos/jolyglot/JolyglotGenericsTest.java
@@ -5,7 +5,10 @@ import java.io.FileWriter;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -127,6 +130,116 @@ public abstract class JolyglotGenericsTest {
     assertThat(jolyglot.toJson(mocks), is(jsonMockArraySample()));
   }
 
+  //Type collections tests.
+
+  @Test public void listToJson() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockList");
+    Type type = method.getGenericReturnType();
+    List<Mock> mocks = new ArrayList<>();
+    mocks.add(new Mock());
+
+    String json = jolyglot.toJson(mocks, type);
+    assertThat(json, is(jsonMockListSample()));
+  }
+
+  @Test public void jsonToList() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockList");
+    Type type = method.getGenericReturnType();
+
+    List<Mock> mocks = jolyglot.fromJson(jsonMockListSample(), type);
+
+    assertThat(jolyglot.toJson(mocks, type),
+        is(jsonMockListSample()));
+  }
+
+  @Test public void parameterizedListToJson() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockParameterizedList");
+    Type type = method.getGenericReturnType();
+
+    List<Mock> mocks = new ArrayList<>();
+    mocks.add(new Mock());
+    MockParameterized<List<Mock>> listMockParameterized = new MockParameterized<>(mocks);
+
+    String json = jolyglot.toJson(listMockParameterized, type);
+
+    try {
+      assertThat(json, is(jsonMockParameterizedListSample()));
+    } catch (AssertionError i) {
+      assertThat(json, is(jsonMockParameterizedListSampleReverse()));
+    }
+  }
+
+  @Test public void jsonToParameterizedList() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockParameterizedList");
+    Type type = method.getGenericReturnType();
+
+    MockParameterized<List<Mock>> listMockParameterized =
+        jolyglot.fromJson(jsonMockParameterizedListSample(), type);
+
+    try {
+      assertThat(jolyglot.toJson(listMockParameterized, type),
+          is(jsonMockParameterizedListSample()));
+    } catch (AssertionError i) {
+      assertThat(jolyglot.toJson(listMockParameterized, type),
+          is(jsonMockParameterizedListSampleReverse()));
+    }
+  }
+
+  @Test public void mapToJson() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockMap");
+    Type type = method.getGenericReturnType();
+    Map<Integer, Mock> mocks = new HashMap<>();
+    mocks.put(1, new Mock());
+
+    String json = jolyglot.toJson(mocks, type);
+    assertThat(json, is(jsonMockMapSample()));
+  }
+
+  @Test public void jsonToMap() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockMap");
+    Type type = method.getGenericReturnType();
+
+    Map<Integer, Mock> mocks = jolyglot.fromJson(jsonMockMapSample(), type);
+
+    assertThat(jolyglot.toJson(mocks, type),
+        is(jsonMockMapSample()));
+  }
+
+  @Test public void parameterizedMapToJson() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockParameterizedMap");
+    Type type = method.getGenericReturnType();
+
+    Map<Integer, Mock> mocks = new HashMap<>();
+    mocks.put(1, new Mock());
+    MockParameterized<Map<Integer, Mock>> mapMockParameterized = new MockParameterized<>(mocks);
+
+    String json = jolyglot.toJson(mapMockParameterized, type);
+
+    try {
+      assertThat(json,
+          is(jsonMockParameterizedMapSample()));
+    } catch (AssertionError i) {
+      assertThat(json,
+          is(jsonMockParameterizedMapSampleReverse()));
+    }
+  }
+
+  @Test public void jsonToParameterizedMap() throws NoSuchMethodException {
+    Method method = Types.class.getDeclaredMethod("mockParameterizedMap");
+    Type type = method.getGenericReturnType();
+
+    MockParameterized<Map<Integer, Mock>> mapMockParameterized =
+        jolyglot.fromJson(jsonMockParameterizedMapSample(), type);
+
+    try {
+      assertThat(jolyglot.toJson(mapMockParameterized, type),
+          is(jsonMockParameterizedMapSample()));
+    } catch (AssertionError i) {
+      assertThat(jolyglot.toJson(mapMockParameterized, type),
+          is(jsonMockParameterizedMapSampleReverse()));
+    }
+  }
+
   private String jsonMockParameterizedSample() {
     return "{\"t\":{\"s1\":\"s1\"},\"s1\":\"s1\"}";
   }
@@ -139,9 +252,39 @@ public abstract class JolyglotGenericsTest {
     return "[{\"s1\":\"s1\"},{\"s1\":\"s1\"}]";
   }
 
+  private String jsonMockListSample() {
+    return "[{\"s1\":\"s1\"}]";
+  }
+
+  private String jsonMockParameterizedListSample() {
+    return "{\"t\":[{\"s1\":\"s1\"}],\"s1\":\"s1\"}";
+  }
+
+  private String jsonMockParameterizedListSampleReverse() {
+    return "{\"s1\":\"s1\",\"t\":[{\"s1\":\"s1\"}]}";
+  }
+
+  private String jsonMockMapSample() {
+    return "{\"1\":{\"s1\":\"s1\"}}";
+  }
+
+  private String jsonMockParameterizedMapSample() {
+    return "{\"t\":{\"1\":{\"s1\":\"s1\"}},\"s1\":\"s1\"}";
+  }
+
+  private String jsonMockParameterizedMapSampleReverse() {
+    return "{\"s1\":\"s1\",\"t\":{\"1\":{\"s1\":\"s1\"}}}";
+  }
+
   private interface Types {
     Mock mock();
     MockParameterized<Mock> mockParameterized();
+
+    List<Mock> mockList();
+    MockParameterized<List<Mock>> mockParameterizedList();
+
+    Map<Integer, Mock> mockMap();
+    MockParameterized<Map<Integer, Mock>> mockParameterizedMap();
   }
 
   protected abstract JolyglotGenerics jolyglot();
